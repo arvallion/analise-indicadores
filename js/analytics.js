b76b75dbf940700b6e73afe8f307437c7a6d8809
arvallion <arvallion.world@gmail.com>
2025-05-22 15:32:29 -0300

    Delete js/analytics.js

--- js/analytics.js
+++
@@ -1,7 +0,0 @@
-// js/analytics.js
-window.dataLayer = window.dataLayer || [];
-function gtag(){ dataLayer.push(arguments); }
-gtag('js', new Date());
-
-// usa o seu Measurement ID
-gtag('config', 'G-EJ9HJJYV43');
